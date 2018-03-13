416e2c6b1f6e339706f052ee2972cadb68247e62
3Xpresso <jeremy.roulland@gmail.com>
2018-03-13 18:11:54 +0100

    Add actions file

---
+++ Inc/actions.h
@@ -0,0 +1,20 @@
+/**
+ * @file    actions.h
+ * @author  Jeremy ROULLAND
+ * @date    13 mar. 2018
+ * @brief   Actions (Simple, Complex, Control, ...)
+ */
+
+/**
+ *    CMD     | OPCOD | / | L | DATA0   DATA1   DATA2   DATA3      |    Comments
+ * Get Inputs	0x05	R	4	Input 1	Input 2	Input 3	Input 4			Ana : 0 à 255	TOR : 0 ou 255
+ * Set speed	0x10	W	2	unit	speed						
+ * Set output	0x11	W	3	unit	subunit	output					
+ */
+
+#ifndef INC_ACTIONS_H_
+#define INC_ACTIONS_H_
+
+
+
+#endif /* INC_ACTIONS_H_ */
--- Inc/traces.h
+++ Inc/traces.h
@@ -5,9 +5,13 @@
  * @brief   Diagnosis
  */
 
+#ifndef INC_TRACES_H_
+#define INC_TRACES_H_
+
 #include "stm32f0xx_hal.h"
 
 void Traces_Init(UART_HandleTypeDef* huart);
 
 void Traces_Process(void);
 
+#endif /* INC_TRACES_H_ */
